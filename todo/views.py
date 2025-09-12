ddca525ac648edc14aae9eaad6773b0080699c64
jerry <jerry@gmail.com>
2025-09-12 21:28:03 +0800

    完成修改功能

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 import json
 from .models import Todo
 from .forms import TodoForm
+from datetime import datetime
 
 
 def create_todo(request):
@@ -19,13 +20,30 @@ def create_todo(request):
 
 
 def view_todo(request, id):
-    todo = None
+    message = ""
+    # 檢視目前
     try:
         todo = Todo.objects.get(id=id)
+        form = TodoForm(instance=todo)
     except Exception as e:
         print(e)
 
-    return render(request, "todo/view-todo.html", {"todo": todo})
+    # 更新資料
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=todo)
+        todo = form.save(commit=False)
+
+        if todo.completed:
+            todo.date_completed = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        else:
+            todo.date_completed = None
+
+        todo.save()
+        message = "更新成功!"
+
+    return render(
+        request, "todo/view-todo.html", {"todo": todo, "form": form, "message": message}
+    )
 
 
 def todolist(request):
