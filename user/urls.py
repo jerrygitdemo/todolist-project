a28c97e0f80eb585bbbd0db9c62a7297afc18fd5
jerry <jerry@gmail.com>
2025-09-17 21:36:35 +0800

    完成登入功能

--- user/urls.py
+++ user/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 
 from django.urls import path
-from .views import user_register
+from .views import user_register, user_login
 
 
 urlpatterns = [
+    path("login/", user_login, name="user-login"),
     path("register/", user_register, name="user-register"),
 ]
